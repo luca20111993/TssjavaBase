71ea4910842400e576c17f4807425fe7882e9fe0
tss <tss@debian-tss24>
2016-11-07 16:04:23 +0100

    Aggiunto input frase utente

--- src/lezione1/Main.java
+++ src/lezione1/Main.java
@@ -5,6 +5,8 @@
  */
 package lezione1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author tss
@@ -15,8 +17,10 @@ public class Main {
     public static void main(String[] args) {
     
         System.out.println("Primo software started .....");
-        
-        
+    
+        String frase = JOptionPane.showInputDialog("Inserisci una frase da stampare");
+        System.out.println(frase);
+               
         
         
         
